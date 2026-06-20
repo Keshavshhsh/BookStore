8c9a1cc09e14a1d9ed284d51dbda44a7503bcce5
keshav kumar <keshavshukla761@gmail.com>
2026-06-21 02:08:45 +0530

    banner and footer pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './index.css'
 import Navbar from './components/Navbar'
-
+import Banner from './components/Banner'
+import Footer from './components/Footer'
 
 function App() {
   return (
    <>
    <Navbar/>
+   <Banner/>
+    <Footer/>
+
 
    </>
   )
